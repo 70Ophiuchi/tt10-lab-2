7597d3c88fc5bc532649e11e193b9742bf40bf0e
70Ophiuchi <60755223+70Ophiuchi@users.noreply.github.com>
2025-02-27 23:41:49 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  custom_logic user_project (
+  priority_encoder user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
